db15c62963265ba14b1fd28392d7f748ef4fe637
Digosgit2 <nerkomal81@gmail.com>
2026-08-23 14:14:04 +0530

    class1 commit 4

--- tests/Class1.spec.js
+++ tests/Class1.spec.js
@@ -1,10 +1,26 @@
-import { test } from '@playwright/test';
+const { test } =require('@playwright/test');
 
-test("check browser", async ({ page, browserName }) => {
+test("TC Name", async ({page})=>{
 
-    console.log("=================================");
-    console.log("Browser:", browserName);
-    console.log("=================================");
+    await page.goto("https://copyright.gov.in/UserRegistration/frmNewUser.aspx");
 
-    await page.goto("https://www.saucedemo.com/");
+    //Select Mr. value
+    await page.locator("//select[@name='ctl00$ContentPlaceHolder1$ddlTitle']").selectOption({index : 1});
+
+    await page.locator("//input[@name='ctl00$ContentPlaceHolder1$txtFName']").fill("Amruta");
+
+    await page.locator("//input[@name='ctl00$ContentPlaceHolder1$txtLName']").fill("Shinde");
+
+    await page.locator("//input[@name='ctl00$ContentPlaceHolder1$txtAddress']").fill("Pune");
+
+    await page.locator("//select[@id='ctl00_ContentPlaceHolder1_ddlCountry']").selectOption({label : 'Canada'});
+
+    await page.locator("//select[@name='ctl00$ContentPlaceHolder1$ddlState']").selectOption({value : '5'});
+
+    await page.locator("//input[@name='ctl00$ContentPlaceHolder1$txtEmail']").fill("amruta@gmail.com");
+
+    await page.getByLabel("Society User").click();
+
+    await page.waitForTimeout(6000);
+
 });
